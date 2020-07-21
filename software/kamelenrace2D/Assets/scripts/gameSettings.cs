de21f9e136f3b0279fbbfe5a1ec438eee1e77e95
kefcom <kefcom@hotmail.com>
2020-07-21 22:12:59 +0200

    added game files (unity3D)

---
+++ software/kamelenrace2D/Assets/scripts/gameSettings.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class gameSettings : MonoBehaviour
+{
+    public float startPos = -7.52f;
+    public float endPos = 6;
+    public float totalDistance = 13.52f;
+    public float totalBasePoints = 30f;
+    public float baseValue = 1f;
+    public float midValue = 2f;
+    public float maxValue = 5f;
+
+    public float baseDistance;
+    public float midDistance;
+    public float maxDistance;
+
+    public bool gameInProgress = false;
+
+    public GameObject kameel1;
+    public GameObject kameel2;
+    public GameObject kameel3;
+    public GameObject kameel4;
+
+    public GameObject topText;
+    public GameObject bottomText;
+
+    private int counter;
+
+
+    public int countDownTime = 10;
+    public int readyTimer = 5;
+
+    public bool canJoin = false;
+
+    public int gameTimeOut = 300;
+
+    public AudioClip a_jingle;
+    public AudioClip a_bugle;
+    public AudioClip a_kameel1;
+    public AudioClip a_kameel2;
+    public AudioClip a_kameel3;
+    public AudioClip a_kameel4;
+    public AudioClip a_runningSound;
+    public AudioClip a_timeout;
+    public AudioClip a_triggerHit;
+    public AudioClip a_winner;
+    public AudioClip a_join;
+
+    private DateTime lastJingle;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //calculate distances
+        totalDistance = endPos - startPos;
+        baseDistance = (totalDistance / totalBasePoints) * baseValue;
+        midDistance = (totalDistance / totalBasePoints) * midValue;
+        maxDistance = (totalDistance / totalBasePoints) * maxValue;
+
+        //initialise join counter
+        counter = countDownTime;
+
+        //start join game state
+        //first time audio
+        gameObject.GetComponent<AudioSource>().loop = false;
+        gameObject.GetComponent<AudioSource>().clip = a_jingle;
+        gameObject.GetComponent<AudioSource>().Play();
+        lastJingle = DateTime.Now;
+        countDownGame();
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F12))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void triggerWin(int playerNumber)
+    {
+        gameInProgress = false;
+        Invoke("playWinSound", 0.5f);
+        bottomText.GetComponent<UnityEngine.UI.Text>().text = "Einde ronde!\nWinnaar: speler " + playerNumber + ".";
+        Invoke("resetCamels", 13f);
+    }
+
+    public void playWinSound()
+    {
+        gameObject.GetComponent<AudioSource>().loop = false;
+        gameObject.GetComponent<AudioSource>().clip = a_winner;
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    private void countDownGame()
+    {
+        if (counter == countDownTime && gameObject.GetComponent<AudioSource>().isPlaying == false)
+        {
+            if ((DateTime.Now - lastJingle).Minutes > 2)
+            {
+                gameObject.GetComponent<AudioSource>().loop = false;
+                gameObject.GetComponent<AudioSource>().clip = a_jingle;
+                gameObject.GetComponent<AudioSource>().Play();
+                lastJingle = DateTime.Now;
+            }
+        }
+        canJoin = true;
+        gameInProgress = false;
+        bottomText.GetComponent<UnityEngine.UI.Text>().text = "Druk op de startknop om mee te spelen\nVolgende ronde start in " + counter + " seconden...";
+        counter--;
+        if (counter == -1)
+        {
+            //check for players
+            if(kameel1.GetComponent<kameelscript>().isPlaying == false && kameel2.GetComponent<kameelscript>().isPlaying == false && kameel3.GetComponent<kameelscript>().isPlaying == false && kameel4.GetComponent<kameelscript>().isPlaying == false)
+            {
+                // no players, reset counter
+                counter = countDownTime;
+                Invoke("countDownGame", 1f);
+            }
+            else
+            {
+                // at least one player joined, moving to next step (readyup)
+                gameObject.GetComponent<AudioSource>().clip = a_bugle;
+                gameObject.GetComponent<AudioSource>().Play();
+                counter = readyTimer;
+                Invoke("readyUp", 0f);
+            }
+        }
+        else
+        {
+            //count down one second...
+            Invoke("countDownGame", 1f);
+        }
+       
+    }
+
+    public void runningSound()
+    {
+        gameObject.GetComponent<AudioSource>().clip = a_runningSound;
+        gameObject.GetComponent<AudioSource>().loop = true;
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    private void readyUp()
+    {
+        canJoin = false;
+        bottomText.GetComponent<UnityEngine.UI.Text>().text = "Klaar om te starten!\nBegin in " + counter + " seconden!";
+        counter--;
+        if(counter == -1)
+        {
+            //start game
+            gameInProgress = true;
+            bottomText.GetComponent<UnityEngine.UI.Text>().text = "START!";
+            Invoke("runningSound", 1f);
+            counter = gameTimeOut;
+            Invoke("countTimeOut", 0f);
+        }
+        else
+        {
+            //count down one second...
+            Invoke("readyUp", 1f);
+        }
+    }
+
+
+    private void resetCamels()
+    {
+
+        //reset camels to begin position
+        kameel1.transform.position = new Vector2(startPos, 0.372f);
+        kameel2.transform.position = new Vector2(startPos, -1.198f);
+        kameel3.transform.position = new Vector2(startPos, -2.698f);
+        kameel4.transform.position = new Vector2(startPos, -4.198f);
+
+        //reset isPlaying vars
+        kameel1.GetComponent<kameelscript>().isPlaying = false;
+        kameel2.GetComponent<kameelscript>().isPlaying = false;
+        kameel3.GetComponent<kameelscript>().isPlaying = false;
+        kameel4.GetComponent<kameelscript>().isPlaying = false;
+
+        //trigger getdown animation when camels are not down already
+        if (kameel1.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("kameel_idle2"))
+        {
+            // do nothing
+        }
+        else
+        {
+            kameel1.GetComponent<kameelscript>().getDown();
+        }
+        if (kameel2.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("kameel_idle2"))
+        {
+            // do nothing
+        }
+        else
+        {
+            kameel2.GetComponent<kameelscript>().getDown();
+        }
+        if (kameel3.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("kameel_idle2"))
+        {
+            // do nothing
+        }
+        else
+        {
+            kameel3.GetComponent<kameelscript>().getDown();
+        }
+        if (kameel4.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("kameel_idle2"))
+        {
+            // do nothing
+        }
+        else
+        {
+            kameel4.GetComponent<kameelscript>().getDown();
+        }
+
+        //reset to game join state
+        counter = countDownTime;
+        Invoke("countDownGame",0f);
+
+    }
+
+    private void countTimeOut()
+    {
+        // if idle during game, reset camels and go back to game join state
+        if(gameInProgress == true)
+        {
+            if (counter == -1)
+            {
+                gameInProgress = false;
+                bottomText.GetComponent<UnityEngine.UI.Text>().text = "Einde spel (timeout).";
+                gameObject.GetComponent<AudioSource>().clip = a_timeout;
+                gameObject.GetComponent<AudioSource>().loop = false;
+                gameObject.GetComponent<AudioSource>().Play();
+                Invoke("resetCamels", 5f);
+            }
+            else
+            {
+                counter--;
+                Invoke("countTimeOut", 1f);
+            }
+
+        }
+    }
+}
---
+++ software/kamelenrace2D/Assets/scripts/kameelscript.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class kameelscript : MonoBehaviour
+{
+    public Animator anim;
+    public int player;
+    public float speed = 5.0f;
+    private Vector2 target;
+    private Vector2 position;
+    private float distance = 0;
+    private GameObject gameSettingsObject;
+    public bool isPlaying = false;
+    public bool firstTarget = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        target = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        gameSettingsObject = GameObject.Find("gameSettings");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        // fix position for getup/getdown animation problem on game end
+        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("kameel_getdown"))
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, step);
+        }
+
+        //get input when game in running and set new position
+        if (gameSettingsObject.GetComponent<gameSettings>().gameInProgress == true && isPlaying == true)
+        {
+            // fix position for getup/getdown animation problem on game start
+            if(firstTarget == true)
+            {
+                target = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+                firstTarget = false;
+            }
+            switch (player)
+            {
+                case 1:
+                    //top score
+                    if (Input.GetKeyUp(KeyCode.X))
+                    {
+                        anim.Play("kameel_running2",-1,0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().maxDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //mid score
+                    if (Input.GetKeyUp(KeyCode.B))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().midDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //low score
+                    if (Input.GetKeyUp(KeyCode.C))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().baseDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+                    break;
+                case 2:
+                    //top score
+                    if (Input.GetKeyUp(KeyCode.D))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().maxDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //mid score
+                    if (Input.GetKeyUp(KeyCode.E))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().midDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //low score
+                    if (Input.GetKeyUp(KeyCode.F))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().baseDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+                    break;
+                case 3:
+                    //top score
+                    if (Input.GetKeyUp(KeyCode.G))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().maxDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //mid score
+                    if (Input.GetKeyUp(KeyCode.H))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().midDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //low score
+                    if (Input.GetKeyUp(KeyCode.I))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().baseDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+                    break;
+                case 4:
+                    //top score
+                    if (Input.GetKeyUp(KeyCode.J))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().maxDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //mid score
+                    if (Input.GetKeyUp(KeyCode.K))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().midDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+
+                    //low score
+                    if (Input.GetKeyUp(KeyCode.L))
+                    {
+                        anim.Play("kameel_running2", -1, 0);
+                        position = gameObject.transform.position;
+                        distance = gameSettingsObject.GetComponent<gameSettings>().baseDistance;
+                        target = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+                        playTriggerHit();
+                        Invoke("trySoundBite", 2f);
+                        break;
+                    }
+                    break;
+            }
+
+
+
+            // move sprite towards the target location
+            if(transform.position.x < target.x)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target, step);
+            }
+            else
+            {
+                anim.Play("kameel_idle_up",-1,0);
+            }
+            
+            //check for winning position
+            if (transform.position.x >= gameSettingsObject.GetComponent<gameSettings>().endPos)
+            {
+                gameSettingsObject.GetComponent<gameSettings>().triggerWin(player);
+            }
+        }
+        else
+        {
+            //stop running animation when game ends
+            if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("kameel_running2"))
+            {
+                anim.Play("kameel_idle_up");
+            }
+
+            //get key inputs for game join
+            if(gameSettingsObject.GetComponent<gameSettings>().canJoin == true && isPlaying == false)
+            {
+                //start button
+                if (Input.GetKeyUp(KeyCode.F1) && player == 1)
+                {
+                    isPlaying = true;
+                    anim.Play("kameel_getup");
+                    target = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+                    firstTarget = true;
+                    gameSettingsObject.GetComponent<AudioSource>().volume = gameSettingsObject.GetComponent<AudioSource>().volume / 4;
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_join;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    Invoke("resetMusicVolume", 1f);
+                }
+                //start button
+                if (Input.GetKeyUp(KeyCode.F2) && player == 2)
+                {
+                    isPlaying = true;
+                    anim.Play("kameel_getup");
+                    target = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+                    firstTarget = true;
+                    gameSettingsObject.GetComponent<AudioSource>().volume = gameSettingsObject.GetComponent<AudioSource>().volume / 4;
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_join;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    Invoke("resetMusicVolume", 1f);
+                }
+                //start button
+                if (Input.GetKeyUp(KeyCode.F3) && player == 3)
+                {
+                    isPlaying = true;
+                    anim.Play("kameel_getup");
+                    target = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+                    firstTarget = true;
+                    gameSettingsObject.GetComponent<AudioSource>().volume = gameSettingsObject.GetComponent<AudioSource>().volume / 4;
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_join;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    Invoke("resetMusicVolume", 1f);
+                }
+                //start button
+                if (Input.GetKeyUp(KeyCode.F4) && player == 4)
+                {
+                    isPlaying = true;
+                    anim.Play("kameel_getup");
+                    target = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+                    firstTarget = true;
+                    gameSettingsObject.GetComponent<AudioSource>().volume = gameSettingsObject.GetComponent<AudioSource>().volume / 2;
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_join;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    Invoke("resetMusicVolume", 1f);
+                }
+            }
+        }
+        
+    }
+
+    public void resetMusicVolume()
+    {
+        gameSettingsObject.GetComponent<AudioSource>().volume = gameSettingsObject.GetComponent<AudioSource>().volume * 4;
+    }
+
+    public void getDown()
+    {
+        //set base target, it will move to this position as long as the animation runs.
+        target = transform.position;
+        anim.Play("kameel_getdown", -1, 0);
+    }
+
+    public void trySoundBite()
+    {
+        int randnr = UnityEngine.Random.Range(1, 10);
+        if (randnr == 5)
+        {
+            int soundbite = UnityEngine.Random.Range(1, 4);
+            switch (soundbite)
+            {
+                case 1:
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_kameel1;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    break;
+                case 2:
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_kameel2;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    break;
+                case 3:
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_kameel3;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    break;
+                case 4:
+                    gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_kameel4;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    break;
+            }
+        }
+    }
+
+    public void playTriggerHit()
+    {
+        gameObject.GetComponent<AudioSource>().clip = gameSettingsObject.GetComponent<gameSettings>().a_triggerHit;
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+}
